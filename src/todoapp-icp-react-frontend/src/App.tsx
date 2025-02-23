11813c127d07ac0babb8ef9c915dc194fb5463d0
Mykhailo <ivchenkomykhailo@gmail.com>
2025-02-23 16:54:26 +0200

    Implement Status and Task types

---
+++ src/todoapp-icp-react-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { todoapp_icp_react_backend } from '../../declarations/todoapp-icp-react-backend';
+
+function App() {
+  return (
+    <main>
+      <img src='/logo2.svg' alt='DFINITY logo' />
+      <br />
+      <br />
+    </main>
+  );
+}
+
+export default App;
